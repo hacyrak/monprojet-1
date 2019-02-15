df983b4f607f9adddc08f43f36a3ef8ee6184e7f
A1007744 <A1007744@a-f004243.salsa.univ-amu.fr>
2019-02-15 14:04:29 +0100

    Non

--- monprojet/src/TP4/monpremierTest.java
+++ monprojet/src/TP4/monpremierTest.java
@@ -2,8 +2,12 @@ package TP4;
 
 public class monpremierTest {
 	private String argument1="Oui";
-	
+	private String argument2="Non";
 	public String toString() {
 		return this.argument1;
 	}
+	public String nottoString() {
+		return this.argument2;
+	}
+	
 }
